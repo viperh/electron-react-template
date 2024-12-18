5aa969ad12601d8aa3d5c019acab4894c10be419
qviperh <qviperh.dev@proton.me>
2024-12-18 18:53:27 +0100

    init dev

---
+++ src/ui/electron/main.ts
@@ -0,0 +1,16 @@
+import {app, BrowserWindow} from "electron";
+import path from "path";
+import {isDev} from "./util.js";
+
+app.on("ready", () => {
+    const mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+
+    if (isDev()) {
+        mainWindow.loadURL('http://localhost:5123')
+    } else {
+        mainWindow.loadFile(path.join(app.getAppPath(), '/dist-react/index.html'))
+    }    
+})
